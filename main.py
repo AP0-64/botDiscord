c11e27cdd68d33ceceb00a7abd3ddc299986a016
Paolo BERTINI <bertin_p@etna-alternance.net>
2026-04-15 23:15:32 +0200

    + #

--- main.py
+++ main.py
@@ -11,11 +11,13 @@ print("Lancement du bot")
 # Initialisation du bot
 bot = discord.Client(intents=discord.Intents.all())
 
+# Événement déclenché lorsque le bot est prêt
 @bot.event
 async def on_ready():
     """Affiche un message dans la console lorsque le bot est prêt"""
     print("bot prêt")
 
+# Récupération du token depuis les variables d'environnement et lancement du bot
 token = os.getenv("DISCORD_TOKEN")
 if not token:
     raise RuntimeError("DISCORD_TOKEN manquant dans le fichier .env")
