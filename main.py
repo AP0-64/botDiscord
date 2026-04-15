b18ee7465ffdeff069a6c181d6d005ef8a121127
Paolo BERTINI <bertin_p@etna-alternance.net>
2026-04-15 23:20:54 +0200

    rename

--- main.py
+++ main.py
@@ -6,18 +6,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-print("Lancement du bot")
+print("Lancement du bot Atlas")
 
-# Initialisation du bot
+# Initialisation du bot Atlas avec tous les intents activés
 bot = discord.Client(intents=discord.Intents.all())
 
 # Événement déclenché lorsque le bot est prêt
 @bot.event
 async def on_ready():
-    """Affiche un message dans la console lorsque le bot est prêt"""
-    print("bot prêt")
+    """Affiche un message dans la console lorsque le bot Atlas est prêt"""
+    print("bot Atlas prêt")
 
-# Récupération du token depuis les variables d'environnement et lancement du bot
+# Récupération du token depuis les variables d'environnement et lancement du bot Atlas
 token = os.getenv("DISCORD_TOKEN")
 if not token:
     raise RuntimeError("DISCORD_TOKEN manquant dans le fichier .env")
